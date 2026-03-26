31aad0fc9ad42a825f7ba33fdf0990ec41138ab9
Felipe <felipeshimizu5@gmail.com>
2026-03-26 00:48:07 -0300

    feat: adiciona JsonCreator no enum Frequencia para aceitar valores case-insensitive

--- src/main/java/com/felipe/habito/enums/Frequencia.java
+++ src/main/java/com/felipe/habito/enums/Frequencia.java
@@ -1,4 +1,13 @@
 package com.felipe.habito.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Frequencia {
+    DIARIO,
+    SEMANAL;
+
+    @JsonCreator
+    public static Frequencia fromString(String value) {
+        return Frequencia.valueOf(value.toUpperCase());
+    }
 }
